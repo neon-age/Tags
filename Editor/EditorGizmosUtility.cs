20a03a6aa3d5681db3babc958043a32b8ec2add1
neon-age <alexander.neonage@gmail.com>
2020-11-12 06:24:03 +0700

    Add missing UNITY_EDITOR preprocessor

--- Editor/EditorGizmosUtility.cs
+++ Editor/EditorGizmosUtility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -43,3 +44,4 @@ public static class EditorGizmosUtility
         }
     }
 }
+#endif
